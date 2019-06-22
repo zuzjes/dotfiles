7de671d64caa2e80a61119552ce5d3576c03e140
Ondrej Sika <ondrej@ondrejsika.com>
2019-06-22 09:20:02 +0200

    Add own bashrc & update apply script

--- apply-mac.sh
+++ apply-mac.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# Bash
-echo "" >> ~/.bashrc
-echo ". ~/.bashrc" >> ~/.profile
-
-echo "" >> ~/.bash_aliases
-echo ". ~/.bash_aliases" >> ~/.profile
-
-
 # ondrejsika/dotfiles (submodule)
 
 # Bash
@@ -15,10 +7,13 @@ echo ". $(pwd)/ondrejsika-dotfiles/core/3rdparty/git-prompt.sh" >> ~/.profile
 echo ". $(pwd)/ondrejsika-dotfiles/core/3rdparty/git-completion.sh" >> ~/.profile
 echo ". $(pwd)/ondrejsika-dotfiles/core/bashrc" >> ~/.profile
 
-# VIM
-rm -rf ~/.vimrc
-ln -s $(pwd)/core/vimrc ~/.vimrc
+# zuzjes/dotfiles
 
-# Hammer Spoon
-rm -rf ~/.hammerspoon
-ln -s $(pwd)/mac/hammerspoon ~/.hammerspoon
+# Bash
+echo "" >> ~/.bashrc
+echo ". ~/.bashrc" >> ~/.profile
+
+echo "" >> ~/.bash_aliases
+echo ". ~/.bash_aliases" >> ~/.profile
+
+echo ". $(pwd)/core/bashrc" >> ~/.profile
